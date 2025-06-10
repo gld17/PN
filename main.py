8cac115df02ace810e1f5f6e87e96564b32b43f2
guold1107 <guold1107@163.com>
2025-06-10 17:07:57 +0800

    add ckpts

--- main.py
+++ main.py
@@ -25,55 +25,59 @@ parser.add_argument('--gpu_id', type=int, default=0)
 params = parser.parse_args()
 
 def cnn_evaluate(model, dataloader):
-    model.eval()
-    total_images = 0
-    correct_1 = 0
-    correct_5 = 0
-    iter = 0
-    with torch.no_grad():
-        for i, data in enumerate(tqdm(dataloader, 0)):
-            images, labels = data
-            images = Variable(images.cuda())
-            labels = Variable(labels.cuda())
-            outputs = model(images)
-            _, predicts_1 = torch.max(outputs.data, 1)
-            _, predicts_5 = outputs.data.topk(5, 1, True, True)
-            total_images += labels.size(0)
-            correct_1 += (predicts_1 == labels).sum().item()
-            correct_5 += (predicts_5 == labels.unsqueeze(1).expand_as(predicts_5)).sum().item()
+	model.eval()
+	total_images = 0
+	correct_1 = 0
+	correct_5 = 0
+	iter = 0
+	with torch.no_grad():
+		for i, data in enumerate(tqdm(dataloader, 0)):
+			images, labels = data
+			images = Variable(images.cuda())
+			labels = Variable(labels.cuda())
+			outputs = model(images)
+			_, predicts_1 = torch.max(outputs.data, 1)
+			_, predicts_5 = outputs.data.topk(5, 1, True, True)
+			total_images += labels.size(0)
+			correct_1 += (predicts_1 == labels).sum().item()
+			correct_5 += (predicts_5 == labels.unsqueeze(1).expand_as(predicts_5)).sum().item()
 
-    return 100 * correct_1 / total_images, 100*correct_5 / total_images
+	return 100 * correct_1 / total_images, 100*correct_5 / total_images
 
 def main():
-    os.environ['CUDA_VISIBLE_DEVICES'] = str(params.gpu_id)
-    device = torch.device("cuda")
-    
-    model, exclude_layers = get_cnn_model(params.model_name)
-    model = model.to(device)
+	os.environ['CUDA_VISIBLE_DEVICES'] = str(params.gpu_id)
+	device = torch.device("cuda")
+	
+	model, exclude_layers = get_cnn_model(params.model_name)
+	model = model.to(device)
 
-    test_dataset = get_dataset(params.dataset, params.model_name)
+	# for CIFAR datasets, load our own model ckpts
+	if 'cifar' in params.dataset:
+		model = torch.load('./ckpts/{}_{}.pt'.format(params.model_name, params.dataset)).to(device)
 
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False, num_workers=4)
+	test_dataset = get_dataset(params.dataset, params.model_name)
 
-    baseline_top1_acc, baseline_top5_acc = cnn_evaluate(model, test_loader)
-    print('Baseline-acc-top1={} Baseline-acc-top5={}'.format(baseline_top1_acc, baseline_top5_acc))
+	test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=False, num_workers=4)
 
-    # quantize model
-    model = quantize_model(model, params, model_name=params.model_name, exclude_layers=exclude_layers)
+	baseline_top1_acc, baseline_top5_acc = cnn_evaluate(model, test_loader)
+	print('Baseline-acc-top1={} Baseline-acc-top5={}'.format(baseline_top1_acc, baseline_top5_acc))
 
-    quantized_top1_acc, quantized_top5_acc = cnn_evaluate(model, test_loader)
-    print('Quantized-acc-top1={} Quantized-acc-top5={}'.format(quantized_top1_acc, quantized_top5_acc))
+	# quantize model
+	model = quantize_model(model, params, model_name=params.model_name, exclude_layers=exclude_layers)
 
-    if params.w_mode == 'pn':
-        log_path = './results/{}_{}_{}{}W{}{}A{}.txt'.format(params.model_name, params.dataset, params.w_mode, params.pn_type, params.w_bit, params.a_mode, params.a_bit)
-    else:
-        log_path = './results/{}_{}_{}W{}{}A{}.txt'.format(params.model_name, params.dataset, params.w_mode, params.w_bit, params.a_mode, params.a_bit)
-    with open(log_path, 'a', encoding='utf-8') as file:
-        file.write('Quantized-acc-top1={} Quantized-acc-top5={} \n'.format(quantized_top1_acc, quantized_top5_acc))
-    
+	quantized_top1_acc, quantized_top5_acc = cnn_evaluate(model, test_loader)
+	print('Quantized-acc-top1={} Quantized-acc-top5={}'.format(quantized_top1_acc, quantized_top5_acc))
+
+	if params.w_mode == 'pn':
+		log_path = './results/{}_{}_{}{}W{}{}A{}.txt'.format(params.model_name, params.dataset, params.w_mode, params.pn_type, params.w_bit, params.a_mode, params.a_bit)
+	else:
+		log_path = './results/{}_{}_{}W{}{}A{}.txt'.format(params.model_name, params.dataset, params.w_mode, params.w_bit, params.a_mode, params.a_bit)
+	with open(log_path, 'a', encoding='utf-8') as file:
+		file.write('Quantized-acc-top1={} Quantized-acc-top5={} \n'.format(quantized_top1_acc, quantized_top5_acc))
+	
 
 
 
 
 if __name__ == "__main__":
-    main()
+	main()
--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 python quant/pn_search.py
 
 # Model Quantization
-python main.py --model_name mobilenetv2 --dataset imagenet --gpu_id 3 --w_bit 4 --a_bit 8 --w_mode pn --a_mode int
+python main.py --model_name mobilenetv2 --dataset cifar100 --gpu_id 3 --w_bit 4 --a_bit 8 --w_mode pn --a_mode int
